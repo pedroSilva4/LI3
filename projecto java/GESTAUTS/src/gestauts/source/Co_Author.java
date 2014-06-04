4b1b520d8bfb6360d73c6c8531870c7bcad5380b
Broas <joaolliveira@gmail.com>
2014-06-04 16:38:03 +0100

    Co_author
    gets sets equals (convem verificar se támais ou menos certo)

--- projecto java/GESTAUTS/src/gestauts/source/Co_Author.java
+++ projecto java/GESTAUTS/src/gestauts/source/Co_Author.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package gestauts.source;
+package src.gestauts.source;
 
 /**
  *
@@ -17,13 +17,47 @@ public class Co_Author {
      
      public Co_Author()
      {
-         
+        this.author="";
+        this.n_times= 0;
      }
-    
-     //implementaar equals
-    //clone
-    //getters e setters
-    //construtores
-    //inc n_times
-    //
-}
+
+    /** Construtor por partes*/
+    public Co_Author(String autor, int times)
+    {   this.author = autor;
+        this.n_times = times;
+    }
+
+    /** Construtor por copia*/
+    public Co_Author(Co_Author coautor)
+    {
+        this.author = coautor.author;
+        this.n_times = coautor.n_times;
+    }
+
+
+
+    /**Métodos de instância*/
+
+    public String daAutor() {return this.author;}
+    public int daNtimes() {return this.n_times;}
+
+    /**Modificadores**/
+    public void setNtimes (int ntimesmod){this.n_times = ntimesmod;}
+
+    /**Cria cópia co_authors*/
+    public Co_Author clone()
+    {
+        return new Co_Author(this);
+    }
+
+    public boolean equals(Co_Author CA)
+    {
+        if (this == CA) return true;
+
+            if((CA == null) || this.getClass() != CA.getClass())
+                return false;
+
+            return this.n_times == CA.n_times && this.author.equals(CA.author);
+    }
+
+    }
