1c5f4f96d96acb87e3c154e357929a3f4af1796b
pedroSilva4 <a64345@alunos.uminho.pt>
2014-06-11 19:35:00 +0100

    tira a mao da pita tone...

---
+++ projecto java/GESTAUTS/src/gestauts/source/MostArticlesComp.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+package gestauts.source;
+
+/**
+ *
+ * @author Pedro
+ */
+
+ 
+import java.util.Comparator;
+ import java.util.Map;
+ 
+ /**
+ - *
+ - * @author Pedro   
+ - */
+ public class MostArticlesComp implements Comparator<Map.Entry<String,Integer>>{
+ 
+     @Override
+     public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+       
+         return (o1.getValue().compareTo(o2.getValue()))*(-1);
+     }
+ 
+     
+     
+ }
+    
+
